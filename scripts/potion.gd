ba6c85422de1baa632892f7c7946614a73483d17
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-03-27 16:25:30 -0700

    other merge fixes

--- scripts/potion.gd
+++ scripts/potion.gd
@@ -4,6 +4,7 @@ class_name Potion
 
 @export var ingredients := []
 @export var draggable := false
+@export var dragging := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	texture = preload("res://stock/potionsotck.png")
@@ -11,8 +12,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if draggable and Input.is_action_pressed("click"):
+	if (draggable or dragging) and Input.is_action_pressed("click"):
 		global_position = get_global_mouse_position()
+		dragging = true	
+	else:
+		if not Input.is_action_pressed("click"):
+			dragging = false
 	
 func _on_area_2d_mouse_entered() -> void:
 	scale = Vector2(1.05, 1.05)
