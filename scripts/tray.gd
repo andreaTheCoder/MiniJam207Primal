6ce1053c966d8bece6f987eea3e760801e31d4c3
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-03-29 17:07:24 -0700

    customer modular text

--- scripts/tray.gd
+++ scripts/tray.gd
@@ -29,6 +29,7 @@ func potion_submit(potionparam, ingredients):
 				temp.queue_free()
 				potionparam.ingredients.clear()
 				AudioPlayer.play_sfx(AudioPlayer.CONFIRMATION, 15)
+				EventBus.add_customer_text.emit()
 				if Global.orders == []:
 					EventBus.out_of_tickets.emit()
 				return
