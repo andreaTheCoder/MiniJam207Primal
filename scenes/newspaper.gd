b0181838de0b877899282a8dc579766c42ee612f
andreaTheCoder <105570399+andreaTheCoder@users.noreply.github.com>
2026-03-29 20:28:24 -0700

    Newspaper add

--- scenes/newspaper.gd
+++ scenes/newspaper.gd
@@ -6,16 +6,12 @@ extends Node2D
 var current_step = 0
 const SHOP = preload("res://scenes/shop.tscn")
 
-var newspaper_images : Array = [
-	"res://art/tray.png", 
-	"res://art/tray.png", 
-	"res://art/tray.png", 
-	"res://art/tray.png", 
-]
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	sprite.texture = load(newspaper_images[Global.day-1])
+func _ready() -> void:
+	name = "res://art/News "
+	name += str(Global.day - 1)
+	name += ".png"
+	sprite.texture = load(name)
 	
 		
