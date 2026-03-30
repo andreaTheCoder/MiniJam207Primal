0277380cafb1cc62ba72b5eae4f0b2d957149f7a
Sophie <sophie.thi.nguyen11@gmail.com>
2026-03-29 17:10:25 -0700

    start screen modifications

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@onready var start_screen_tutorial_button: Button = $CanvasLayer/StartScreenTutorialButton
-@onready var start_screen_button: Button = $CanvasLayer/StartScreenButton
+@onready var start_screen_tutorial_button: Button = $CanvasLayer/VBoxContainer/StartScreenTutorialButton
+@onready var start_screen_button: Button = $CanvasLayer/VBoxcontainer/StartScreenButton
 const SHOP = preload("res://scenes/shop.tscn")
 
 # Called when the node enters the scene tree for the first time.
